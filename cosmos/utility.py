eba89cdfa61d89bfd15a86ca6e6c24eed8c2b616
Samuel Yanez <samuelyanez94@gmail.com>
2019-08-03 17:42:20 -0700

    Add function to get thumbnails from videos

--- cosmos/utility.py
+++ cosmos/utility.py
@@ -12,6 +12,7 @@ Created on August 3, 2019
 import requests
 import logging
 import datetime
+import re
 
 # Logging
 logging.basicConfig(level=logging.DEBUG)
@@ -36,8 +37,6 @@ def _get_apod(date=None, thumbnails=False):
 
     return requests.get(PATH, parameters).json()
 
-
-
 def _parse(date=None, thumbnails=False):
     """
     Accepts a date in '%Y-%m-%d' format. Returns the URL of the APOD image of that day.
@@ -45,3 +44,21 @@ def _parse(date=None, thumbnails=False):
     logging.info("'_parse' called")
 
     return _get_apod(date, thumbnails)
+
+def _get_thumbnails(url):
+    video_thumb = ""
+    if "youtube" in url or "youtu.be" in url:
+        # Get ID from YouTube URL
+        youtube_id_regex = re.compile("(?:(?<=(v|V)/)|(?<=be/)|(?<=(\?|\&)v=)|(?<=embed/))([\w-]+)")
+        video_id = youtube_id_regex.findall(url)
+        video_id = ''.join(''.join(elements) for elements in video_id).replace("?", "").replace("&", "")
+        video_thumb = "https://img.youtube.com/vi/" + video_id + "/maxresdefault.jpg"
+    elif "vimeo" in url:
+        # Get ID from Vimeo URL
+        vimeo_id_regex = re.compile("(?:/video/)(\d+)")
+        vimeo_id = vimeo_id_regex.findall(url)[0]
+        # Make API call to get thumbnail URL
+        response = requests.get("https://vimeo.com/api/v2/video/" + vimeo_id + ".json").json()
+        video_thumb = response['thumbnail_large']
+
+    return video_thumb
